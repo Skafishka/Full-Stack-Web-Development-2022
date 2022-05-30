f25c0d6567f7c397956c82b1b45727a30b86b931
Skafishka <sergey.kolygin@gmail.com>
2022-05-30 15:41:50 +0300

    1.7: unicafe step2

--- Part-1/unicafe/src/App.js
+++ Part-1/unicafe/src/App.js
@@ -15,6 +15,7 @@ const Statistics = (props) => {
       <p>good {props.good}</p>
       <p>neutral {props.neutral}</p>
       <p>bad {props.bad}</p>
+      <p>all {props.all}</p>
     </>
   )
 }
@@ -22,24 +23,34 @@ const Statistics = (props) => {
 const App = () => {
 const appName = 'give feedback'
 const appStatistics = 'statistics'
-  // save clicks of each button to its onw state
+
 const [good, setGood] = useState(0)
 const [neutral, setNeutral] = useState(0)
 const [bad, setBad] = useState(0)
+const [allClicks, setAll] = useState([])
+
+const goodClick = () => {
+  setAll(allClicks.concat('1'))
+  setGood(good + 1)
+}
+
+const neutralClick = () => {
+  setAll(allClicks.concat('1'))
+  setNeutral(neutral + 1)
+}
+
+const badClick = () => {
+  setAll(allClicks.concat('1'))
+  setBad(bad + 1)
+}
 
   return (
     <div>
       <Header appName={appName} />
-      <button onClick={() => setGood(good + 1)}>
-        good
-      </button>
-      <button onClick={() => setNeutral(neutral + 1)}>
-        neutral
-      </button>
-      <button onClick={() => setBad(bad + 1)}>
-        bad
-      </button>
-      <Statistics appStatistics={appStatistics} good={good} neutral={neutral} bad={bad}/> 
+      <button onClick={goodClick}>good</button>
+      <button onClick={neutralClick}>neutral</button>
+      <button onClick={badClick}>bad</button>
+      <Statistics appStatistics={appStatistics} good={good} neutral={neutral} bad={bad} all={allClicks.length}/> 
     </div>
   );
 }
