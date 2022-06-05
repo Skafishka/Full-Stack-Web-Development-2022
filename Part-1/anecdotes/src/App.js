54aaeeae4c0dfaf8cc22ebe7ac646500a4a5f5e0
SKo <sergey.kolygin@gmail.com>
2022-06-05 17:44:23 +0300

    1.12*: anecdotes step1

---
+++ Part-1/anecdotes/src/App.js
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>{props.text}</button>
+)
+
+const Text = (props) => {
+  return (
+    <>
+      <p>{props.anecdotes}</p>
+    </>
+  )
+}
+
+const App = () => {
+  const nextAnecdote = () => {
+    setSelected(Math.floor(Math.random() * anecdotes.length))
+  }
+  const [selected, setSelected] = useState(0)
+
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software projects makes it later',
+    'The best way to get a project done faster is to start sooner',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'How does a project get to be a year late?... One day at a time.',
+    'Every good work of software starts by scratching a developers personal itch',
+    'Prolific programmers contribute to certain disaster',
+    'It is better to wait for a productive programmer to become available than it is to wait for the first available programmer to become productive',
+    'Simplicity is prerequisite for reliability',
+    'Correctness is clearly the prime quality. If a system does not do what it is supposed to do, then everything else about it matters little.',
+    'If you cannot grok the overall structure of a program while taking a shower, you are not ready to code it.',
+    'If something is worth doing once, its worth building a tool to do it.',
+    'Conceptual integrity is the most important consideration in system design.'
+  ]
+
+  return (
+    <>
+      <Text anecdotes={anecdotes[selected]}/>
+      <Button text='Next anecdote' handleClick={nextAnecdote}/>
+     
+    </>
+  )
+}
+
+export default App;
--- Part-1/unicafe/src/App.js
+++ Part-1/unicafe/src/App.js
@@ -88,36 +88,36 @@ const Statistics = (props) => {
 }
 
 const App = () => {
-const appName = 'give feedback'
-const appStatistics = 'statistics'
-const [good, setGood] = useState(0)
-const [neutral, setNeutral] = useState(0)
-const [bad, setBad] = useState(0)
-const [allClicks, setAll] = useState([])
-const [number, setAverage] = useState(0)
-const [valueQ, setValue] = useState(0)
+  const appName = 'give feedback'
+  const appStatistics = 'statistics'
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [allClicks, setAll] = useState([])
+  const [number, setAverage] = useState(0)
+  const [valueQ, setValue] = useState(0)
 
-const goodClick = () => {
-  setAll(allClicks.concat(1))
-  setGood(good + 1)
-  setAverage(number + 1)
-}
+  const goodClick = () => {
+    setAll(allClicks.concat(1))
+    setGood(good + 1)
+    setAverage(number + 1)
+  }
 
-const neutralClick = () => {
-  setAll(allClicks.concat(0))
-  setNeutral(neutral + 1)
-  setAverage(number + 0)
-}
+  const neutralClick = () => {
+    setAll(allClicks.concat(0))
+    setNeutral(neutral + 1)
+    setAverage(number + 0)
+  }
 
-const badClick = () => {
-  setAll(allClicks.concat(-1))
-  setBad(bad + 1)
-  setAverage(number - 1)
-}
+  const badClick = () => {
+    setAll(allClicks.concat(-1))
+    setBad(bad + 1)
+    setAverage(number - 1)
+  }
 
-const submitFeedback = () => {
-  setValue(valueQ + 1)
-}
+  const submitFeedback = () => {
+    setValue(valueQ + 1)
+  }
 
   return (
     <div className='App'>
@@ -127,7 +127,6 @@ const submitFeedback = () => {
       <Button text="bad" handleClick={badClick} />
       <Statistics appStatistics={appStatistics} allClicks={allClicks.length} good={good} neutral={neutral} bad={bad} all={allClicks.length} average={number / allClicks.length} positive={good * 100 / allClicks.length} valueQ={valueQ}/>
       <Button text="Submitting feedback" handleClick={submitFeedback} />
-
     </div>  
   )
 }
