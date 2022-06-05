3750fc325091c220d25ba2267ba1097b5e76b153
SKo <sergey.kolygin@gmail.com>
2022-06-06 01:43:56 +0300

    1.13*: anecdotes step2

--- Part-1/anecdotes/src/App.js
+++ Part-1/anecdotes/src/App.js
@@ -8,52 +8,57 @@ const Text = (props) => {
   return (
     <>
       <p>{props.anecdotes}</p>
+      <p>The anecdote has {props.value} votes</p>
     </>
   )
 }
 
-const App = () => {
-  const anecdotes = [
-    'If it hurts, do it more often',
-    'Adding manpower to a late software projects makes it later',
-    'The best way to get a project done faster is to start sooner',
-    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'How does a project get to be a year late?... One day at a time.',
-    'Every good work of software starts by scratching a developers personal itch',
-    'Prolific programmers contribute to certain disaster',
-    'It is better to wait for a productive programmer to become available than it is to wait for the first available programmer to become productive',
-    'Simplicity is prerequisite for reliability',
-    'Correctness is clearly the prime quality. If a system does not do what it is supposed to do, then everything else about it matters little.',
-    'If you cannot grok the overall structure of a program while taking a shower, you are not ready to code it.',
-    'If something is worth doing once, its worth building a tool to do it.',
-    'Conceptual integrity is the most important consideration in system design.'
-  ]
+const Anecdotes = [
+  '0. If it hurts, do it more often',
+  '1. Adding manpower to a late software projects makes it later',
+  '2. The best way to get a project done faster is to start sooner',
+  '3. The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  '4. How does a project get to be a year late?... One day at a time.',
+  '5. Every good work of software starts by scratching a developers personal itch',
+  '6. Prolific programmers contribute to certain disaster',
+  '7. It is better to wait for a productive programmer to become available than it is to wait for the first available programmer to become productive',
+  '8. Simplicity is prerequisite for reliability',
+  '9. Correctness is clearly the prime quality. If a system does not do what it is supposed to do, then everything else about it matters little.',
+  '10. If you cannot grok the overall structure of a program while taking a shower, you are not ready to code it.',
+  '11. If something is worth doing once, its worth building a tool to do it.',
+  '12. Conceptual integrity is the most important consideration in system design.'
+]
 
+const points = Array.from({length: Anecdotes.length}).fill(0)
+const copy = points
+
+
+const App = () => {
+  
   const [selected, setSelected] = useState(0)
   const [value, setValue] = useState(0)
-  const [click, setClick] = useState(0)
-
+  
   const vote = () => {
-    setValue(value + 1)
-    setClick(click + 1)
+    copy[selected] += 1
+    setValue(copy[selected])
+    console.log(copy[selected])
+    console.log(copy)
+    console.log("votes " + value)
   }
 
   const nextAnecdote = () => {
-    setSelected(Math.floor(Math.random() * anecdotes.length))
-  }
-
-  const points = Array.apply(null, {length: anecdotes.length}).map(function() {return 0;})
-  const copy = points
-  copy[selected] += click
- 
+    
+    setSelected(Math.floor(Math.random() * Anecdotes.length))   
+    
+  } 
   
-  console.log(copy)
-   
   return (
     <>
-      <Text anecdotes={anecdotes[selected]}/>
+      <Text anecdotes={Anecdotes[selected]} value={value}/>
+      
       <Button text='vote' handleClick={vote}/>
-      <Button text='Next anecdote' handleClick={nextAnecdote}/>  
+      <Button text='Next anecdote' handleClick={nextAnecdote}/>
+      
     </>
   )
 }
