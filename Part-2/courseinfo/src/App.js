4a2658eb80648c457bf1496b2983b1cd06466c03
SKo <sergey.kolygin@gmail.com>
2022-06-16 07:38:41 +0300

    2.5: separate module -not finished yet

--- Part-2/courseinfo/src/App.js
+++ Part-2/courseinfo/src/App.js
@@ -1,12 +1,12 @@
-import React from "react"
 
-const Result = (props) => {
+
+const App = ({courses}) => {
   return (
     <div>
       <ul>
         <h1>Web development curriculum</h1>
       </ul>
-      {props.courses.map(note => 
+      {courses.map(note =>
         <ul key={note.id}>
           <h2>{note.name}</h2>
           <div>
@@ -21,63 +21,7 @@ const Result = (props) => {
       )}
     </div>
   )
-}
-
-const Course = ({courses}) => {
-  return (
-    <>
-     <Result courses={courses} />
-    </>
-  )  
-}
-
-const App = () => {
-  const courses = [
-    {
-      id: 1,
-      name: 'Half Stack application development',
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3
-        },
-        {
-          name: 'Redux',
-          exercises: 11,
-          id: 4
-        }
-      ]
-    },
-    {
-      name: 'Node.js',
-      id: 2,
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id:2
-        }
-      ]
-    }
-  ]
-      
-  return <Course courses={courses} />
+  
 }
 
 export default App;
