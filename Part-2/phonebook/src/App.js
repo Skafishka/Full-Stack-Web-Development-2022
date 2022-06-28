9cb61698d28f1ce8d6aee6ebe73e5364c2d382d1
Skafishka <sergey.kolygin@gmail.com>
2022-06-28 23:17:36 +0300

    2.8: The Phonebook Step3

--- Part-2/phonebook/src/App.js
+++ Part-2/phonebook/src/App.js
@@ -2,22 +2,32 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas" }
+    { name: 'Arto Hellas',
+      number: '9891830' }
   ])
   const [newName, setNewName] = useState('')
+  const [newPhone, setNewPhone] = useState('')
 
   const addNote = (event) => {
     event.preventDefault()
     const noteObject = {
       name: newName,
+      number: newPhone,
     }
-    persons.find(q => {return JSON.stringify(q.name) === JSON.stringify(newName);}) !== undefined ? window.alert(`${newName} is already added to phonebook`) : setPersons(persons.concat(noteObject))  
+    persons.find(q => {return (JSON.stringify(q.name) === JSON.stringify(newName)) || (JSON.stringify(q.number) === JSON.stringify(newPhone))}) 
+      !== undefined 
+      ? window.alert(`${newName} or ${newPhone} are already added to phonebook`) 
+      : setPersons(persons.concat(noteObject))
     setNewName('')
+    setNewPhone('')
     }
   
   const handleNoteChange = (event) => {
-    console.log(event.target.value)
     setNewName(event.target.value)
+  }
+
+  const handlePhoneChange = (event) => {
+    setNewPhone(event.target.value)
   }
 
   return (
@@ -25,9 +35,17 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={addNote}>
         <div>
+          name: 
           <input 
             value={newName}
             onChange={handleNoteChange}
+          />
+        </div>
+        <div>
+          number:
+          <input
+            value={newPhone}
+            onChange={handlePhoneChange}
           />
         </div>
         <div>
@@ -37,9 +55,9 @@ const App = () => {
       <h2>Numbers</h2>
         {persons.map((element, index) => 
           <li key={index.toString()}>
-            {element.name}
+            {element.name} {element.number}
           </li>
-        )}
+        )}        
     </div>
   );
 }
