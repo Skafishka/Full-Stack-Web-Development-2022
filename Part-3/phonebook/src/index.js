7edfc5d9a826cf6c6e82d004dc99acfffe91e741
SKo <sergey.kolygin@gmail.com>
2022-08-23 23:56:51 +0300

    3.15: Phonebook database, step3

--- Part-3/phonebook/src/index.js
+++ Part-3/phonebook/src/index.js
@@ -1,45 +1,39 @@
-require('dotenv').config()
 const express = require('express')
-const morgan = require('morgan')
 const app = express()
-const Person = require('./models/person.js')
+const cors = require('cors')
+require('dotenv').config()
+const morgan = require('morgan')
+const Person = require('./models/person')
+const { request, response } = require('express')
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path: ', request.path)
+  console.log('Body: ', request.body)
+  console.log('---')
+  next()
+}
 
 app.use(express.json())
+
+app.use(requestLogger)
+
+app.use(cors())
+
 app.use(express.static('build'))
+
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
-
-app.get('/api/persons', (request, response) => {
-  Person.find({}).then(persons => {
-    response.json(persons)
-  })
-})
-
-app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(person => {
-    response.json(person)
-  })
-})
-
-app.get('/q', (req, res) => {
-  Person.find({}).sort( {id : -1} ).then(q => {res.json(q)})
-})
-
-/* deprecated
-const generateId = () => {
-  const maxId = Person.estimatedDocumentCount() > 0
-  ? Person.estimatedDocumentCount() + 1
-  : 1
-  return maxId
-}
-*/
 
 app.post('/api/persons', (request, response) => {
   const body = request.body
-  
+  if (body.name === '') {
+    return response.status(400).json({ error: 'name missing' })
+  }
+
   const person = new Person({
-    id: body.id,
     name: body.name,
     number: body.number,
+    id: body.id,
   })
 
   person.save().then(savedPerson => {
@@ -47,18 +41,56 @@ app.post('/api/persons', (request, response) => {
   })
 })
 
-app.get('/info', (request, response) => {
-  Person.find().estimatedDocumentCount(function(result) {
-    response.send(result)
+app.get('/api/persons', (request, response) => {
+  Person.find({}).then(persons => {
+    response.json(persons)
   })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.get('/info', (request, response) => {
+  Person.find({}).then(rsl => {
+    response.json(`Base contains: ${rsl.length} contacts`)
+  })
+})
+
+
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
+
+app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndRemove(request.params.id)
     .then(result => {
-      response.status(204).end
+      response.status(204).end()
     })
+    .catch(error => next(error))
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
--- Part-3/phonebook/src/models/person.js
+++ Part-3/phonebook/src/models/person.js
@@ -4,14 +4,14 @@ const url = process.env.MONGODB_URI
 mongoose.connect(url)
 
 const personSchema = new mongoose.Schema({
-  id: Number,
   name: String,
   number: String,
+  id: Number
 })
   
 personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id
+    returnedObject.id = returnedObject._id.toString()
     delete returnedObject._id
     delete returnedObject.__v
   }
