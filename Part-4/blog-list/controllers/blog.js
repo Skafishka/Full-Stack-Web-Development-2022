dd70edbc17450867c7a3ac1a9f481a5f41feedaf
SKo <sergey.kolygin@gmail.com>
2022-08-29 13:11:47 +0300

    fix by eslint

--- Part-4/blog-list/controllers/blog.js
+++ Part-4/blog-list/controllers/blog.js
@@ -1,34 +1,34 @@
 const blogesRouter = require('express').Router()
 const Blog = require('../models/blog')
- 
-  blogesRouter.get('/', (request, response) => {
-    Blog
-      .find({})
-      .then(blogs => {
-        if (blogs) {
-          response.json(blogs)
-        } else {
-          response.status(404).end()
-        }
-      })
-      .catch(error => next(error))
+
+blogesRouter.get('/', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      if (blogs) {
+        response.json(blogs)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
+
+blogesRouter.post('/', (request, response, next) => {
+  const body = request.body
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
   })
 
-  blogesRouter.post('/', (request, response, next) => {
-    const body = request.body
-  
-    const blog = new Blog({
-      title: body.title,
-      author: body.author,
-      url: body.url,
-      likes: body.likes,
+  blog.save()
+    .then(result => {
+      response.status(201).json(result)
     })
-  
-    blog.save()
-      .then(result => {
-        response.status(201).json(result)
-      })
-      .catch(error => next(error))
-  })
-  
+    .catch(error => next(error))
+})
+
 module.exports = blogesRouter
