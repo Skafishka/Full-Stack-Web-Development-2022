915a7a9a134cfb50f3b1c77e6638c36f68e08a00
SKo <sergey.kolygin@gmail.com>
2022-09-02 13:41:50 +0300

    4.14 Blog list expansions, step2

--- Part-4/blog-list/tests/blog_api.test.js
+++ Part-4/blog-list/tests/blog_api.test.js
@@ -31,73 +31,75 @@ describe('checking the initial condition of blogs', () => {
 
     expect(response.body[0].title).toBe('Arthur')
   })
-})
 
-describe('adding blogs', () => {
-  test('creating a new blog', async () => {
-    const newBlog = {
-      title: 'How to add a new Blog',
-      author: 'Wheil Conrad',
-      url: 'www.qwerty.com',
-      likes: 55
-    }
 
-    await api
-      .post('/api/blogs')
-      .send(newBlog)
-      .expect(201)
-      .expect('Content-Type', /application\/json/)
+  describe('adding blogs', () => {
+    test('creating a new blog', async () => {
+      const newBlog = {
+        title: 'How to add a new Blog',
+        author: 'Wheil Conrad',
+        url: 'www.qwerty.com',
+        likes: 55
+      }
 
-    const blogsAtEnd = await helper.blogsInDb()
-    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
 
-    const title = blogsAtEnd.map(q => q.title)
-    expect(title).toContain('How to add a new Blog')
+      const blogsAtEnd = await helper.blogsInDb()
+      expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+
+      const title = blogsAtEnd.map(q => q.title)
+      expect(title).toContain('How to add a new Blog')
+    })
   })
-})
 
-describe('4.9*: Blog list tests, step2', () => {
-  test('verifies that the unique identifier property posts is id', async () => {
-    const response = await api.get('/api/blogs')
+  describe('4.9*: Blog list tests, step2', () => {
+    test('verifies that the unique identifier property posts is id', async () => {
+      const response = await api.get('/api/blogs')
 
-    expect(response.body[0].id).toBeDefined()
+      expect(response.body[0].id).toBeDefined()
+    })
   })
-})
 
-describe('deletion of a blog', () => {
-  test('succeeds with status code 204', async () => {
-    const blogsAtStart = await helper.blogsInDb()
-    const blogToDelete = blogsAtStart[0]
+  describe('deletion of a blog', () => {
+    test('succeeds with status code 204', async () => {
+      const blogsAtStart = await helper.blogsInDb()
+      const blogToDelete = blogsAtStart[0]
 
-    await api
-      .delete(`/api/blogs/${blogToDelete.id}`)
-      .expect(204)
+      await api
+        .delete(`/api/blogs/${blogToDelete.id}`)
+        .expect(204)
 
-    const blogsAtEnd = await helper.blogsInDb()
+      const blogsAtEnd = await helper.blogsInDb()
 
-    expect(blogsAtEnd).toHaveLength(
-      blogsAtStart.length - 1
-    )
+      expect(blogsAtEnd).toHaveLength(
+        blogsAtStart.length - 1
+      )
 
-    const titles = blogsAtEnd.map(q => q.content)
+      const titles = blogsAtEnd.map(q => q.content)
 
-    expect(titles).not.toContain(blogToDelete.title)
+      expect(titles).not.toContain(blogToDelete.title)
+    })
   })
-})
 
-describe('update a likes amoount', () => {
-  test('put succeeds', async () => {
-    const blogsAtStart = await helper.blogsInDb()
-    const blogToUpdate = blogsAtStart[1]
-    const updateLikes = { likes: 56 }
+  describe('update a likes amoount', () => {
+    test('put succeeds', async () => {
+      const Start = await helper.blogsInDb()
+      const blogToUpdate = Start[1]
+      const updateLikes = { likes: 56 }
 
-    await api
-      .put(`/api/blogs/${blogToUpdate.id}`)
-      .send(updateLikes)
-      .expect(200)
+      await api
+        .put(`/api/blogs/${blogToUpdate.id}`)
+        .send(updateLikes)
+        .expect(200)
 
-    const likesNumber = blogsAtStart.map(q => q.likes)
-    expect(likesNumber).toContain(56)
+      const End = await helper.blogsInDb()
+      const likesNumber = End.map(q => q.likes)
+      expect(likesNumber).toContain(56)
+    })
   })
 })
 
