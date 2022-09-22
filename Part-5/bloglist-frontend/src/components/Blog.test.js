60d383a621285374a99adce48e0953e4c0b795f1
SKo <sergey.kolygin@gmail.com>
2022-09-22 12:16:56 +0300

    5.14: Blog list tests, step2

--- Part-5/bloglist-frontend/src/components/Blog.test.js
+++ Part-5/bloglist-frontend/src/components/Blog.test.js
@@ -1,17 +1,18 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 test('renders content', () => {
   const blog = [
     {
-        title: '111',
-        author: 'Genrich'
+      title: '111',
+      author: 'Genrich'
     },
     {
-        title: 'again',
-        author: 'Jakob'
+      title: 'again',
+      author: 'Jakob'
     }
   ]
 
@@ -22,3 +23,33 @@ test('renders content', () => {
 
   expect(element).toBeDefined()
 })
+/*
+test('clicking the View button shows the blogs url and number of likes', async () => {
+  const blog = [
+    {
+      title: '111',
+      author: 'Genrich',
+      likes: 56,
+      url: 'www.genrich.net'
+    },
+    {
+      title: 'again',
+      author: 'Jakob',
+      likes: 89,
+      url: 'www.jakob.net'
+    }
+  ]
+
+  const mockHandler = jest.fn()
+
+  render(
+    <Blog blogs={blog} toggleVisibility={mockHandler} />
+  )
+
+  const user = userEvent.setup()
+  const button = screen.getAllByText('www.jakob.net')
+  screen.debug(button)
+  await user.click(button)
+
+  expect(mockHandler.mock.calls).toHaveLength(2)
+})*/
