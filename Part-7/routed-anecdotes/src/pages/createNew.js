34eeee2411073a596b557646b1118882ff4966fb
Skafishka <sergey.kolygin@gmail.com>
2022-12-07 14:03:10 +0200

    7.6: anecdotes and hooks step3

--- Part-7/routed-anecdotes/src/pages/createNew.js
+++ Part-7/routed-anecdotes/src/pages/createNew.js
@@ -2,48 +2,48 @@ import { useField } from '../hooks'
 import { useNavigate } from 'react-router-dom'
 
 const CreateNew = (props) => {
-    const {...content} = useField('content')
-    const {...author} = useField('author')
-    const {...info} = useField('info')
+  const {reset: resetContent, ...content} = useField('content')
+  const {reset: resetAuthor, ...author} = useField('author')
+  const {reset: resetInfo, ...info} = useField('info')
   
-    const navigate = useNavigate()
+  const navigate = useNavigate()
   
-    const handleSubmit = (e) => {
-      e.preventDefault()
-      props.addNew({
-        content: content.value,
-        author: author.value,
-        info: info.value,
-        votes: 0
-      })
-      navigate('/')
-    }
-
-    const handleSubmitReset = (e) => {
-      e.preventDefault()
-      content.reset()
-      author.reset()
-      info.reset()
-    }
-  
-    return (
-      <div>
-        <h2>create a new anecdote</h2>
-        <form onSubmit={handleSubmit}>
-          content:
-          <input {...content} />
-          <br/>
-          author:
-          <input {...author} />
-          <br/>
-          url for more info:
-          <input {...info} />
-          <br />
-          <button>create</button>
-          <button onClick={handleSubmitReset}>reset</button>
-        </form>
-      </div>
-    )
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    props.addNew({
+      content: content.value,
+      author: author.value,
+      info: info.value,
+       votes: 0
+    })
+    navigate('/')
   }
+
+  const handleSubmitReset = (e) => {
+    e.preventDefault()
+    resetContent()
+    resetAuthor()
+    resetInfo()
+  }
+  
+  return (
+    <div>
+      <h2>create a new anecdote</h2>
+      <form onSubmit={handleSubmit}>
+        content:
+        <input {...content} />
+        <br/>
+        author:
+        <input {...author} />
+        <br/>
+        url for more info:
+        <input {...info} />
+        <br />
+        <button>create</button>
+        <button onClick={handleSubmitReset}>reset</button>
+      </form>
+    </div>
+  )
+}
 
 export default CreateNew
